e886f24f64597c997657412bb8c9be9e78fae200
shrxth-ux <atharvshreashth@gmail.com>
2026-09-08 22:53:01 +0530

    Add program to calculate sum of two integers from user

---
+++ Day-4(2).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+int a;
+  printf ("enter first digit\n");
+scanf ("%d" , &a); 
+int b; 
+printf ("enter second digit\n");
+scanf ("%d" , &b);
+int sum;
+sum = a+b;
+printf ("sum of the two digits is %d\n" ,sum);
+return 0;
+}
