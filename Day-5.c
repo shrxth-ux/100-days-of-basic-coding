0de8391c28fc017b5ec88b9a0138736a0ab52fc5
shrxth-ux <atharvshreashth@gmail.com>
2026-09-09 22:11:26 +0530

    Add programs for rectangle perimeter and circle circumference
    This file contains two separate programs: one to calculate the perimeter of a rectangle and another to calculate the circumference of a circle.

---
+++ Day-5.c
@@ -0,0 +1,26 @@
+perimeter of rectangle 
+#include <stdio.h>
+int main ()
+{
+  float l;
+printf ("enter leangth\n");
+  scanf ("%f",&l);
+  float b;
+printf ("enter breath\n");
+  scanf ("%f",&b);
+  float perimeter = 2*(l+b); 
+printf ("perimeter of rectangle is %f.\n", perimeter);
+return 0;
+}
+
+circumference of a circle
+#include<stdio.h>
+int main()
+{
+float r;
+printf ("enter the radius\n");
+scanf ("%f",&r);
+float circumference = 2*3.14*r;
+printf ("the circumference of the circle is %f", circumference);
+return 0;
+}
